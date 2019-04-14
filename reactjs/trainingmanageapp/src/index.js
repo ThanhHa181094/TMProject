47114d5764944c260de233af741fe5e84dfc45a4
dauthang81197 <thangdau811@gmail.com>
2019-04-14 21:28:06 +0700

    add deleteArticle

--- reactjs/trainingmanageapp/src/index.js
+++ reactjs/trainingmanageapp/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import HomeComponent from './HomeComponent';
+import DeleteComponent from './DeleteComponent';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<HomeComponent />, document.getElementById('root'));
+ReactDOM.render(<DeleteComponent />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
